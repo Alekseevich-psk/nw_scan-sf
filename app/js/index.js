8b2b6cf3b4ea928c15bf0061a9cefaa662aff0f3
Andrew <80355579+Alekseevich-psk@users.noreply.github.com>
2022-12-15 17:27:59 +0300

    start react

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,9 @@
 import "./common/animation-picture";
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+const root = ReactDOM.createRoot(document.getElementById("app"));
+
+import App from "./components/App.jsx";
+
+root.render(<App />);
