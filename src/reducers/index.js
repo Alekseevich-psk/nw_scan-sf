6b9205518ebd8a13cb6ef06de7f9946b986b5b82
Andrew <80355579+Alekseevich-psk@users.noreply.github.com>
2023-02-03 14:13:33 +0300

    deleted task reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from "redux";
 import authStore from './auth';
-import taskStore from './task';
 
 export default combineReducers({
-    authStore, 
-    taskStore
+    authStore,
 })
