437972369c71bfa727f78e14c07f80f09d9c3f90
Andrew <80355579+Alekseevich-psk@users.noreply.github.com>
2023-02-20 16:08:02 +0300

    fixed

--- src/hooks/parserXml.jsx
+++ src/hooks/parserXml.jsx
@@ -1,26 +1,5 @@
-const XMLParser = require('react-xml-parser');
-
 export default function parserXml(value) {
-    // const xml = new XMLParser().parseFromString(value); 
-    // const obj = xml.children['0'].children;
-    // const res = [];
-
-    // for (const key in obj) {
-    //     res.push(obj[key].value);
-    // }
-
-    // return res.toString().replace(/[^а-яёА-ЯЁ ]/g, "");
-
-
-    // if (value)
-    //     return value;
-    // else if (XMLSerializer) {
-    //     var xml_serializer = new XMLSerializer();
-    //     return xml_serializer.serializeToString(value);
-    // }
-    // else {
-    //     alert("ERROR: Extremely old browser");
-    //     return "";
-    // }
+    let subRes = value.replace(/(<([^>]+)>)/ig, "");
+    return subRes.replace(/['&lt;''br&gt;''br&gt;''/'aA-zZ]/g, "").slice(0, 300) + '...';
 }
 
